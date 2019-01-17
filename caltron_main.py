b08ce86b7dedaa66c2d041de4eb93caccb2feaa1
mahesh-tripathi <mahesh_tripathi@yahoo.com>
2019-01-18 02:35:14 +0530

    initialised main_func

--- caltron_main.py
+++ caltron_main.py
@@ -1,1 +1,8 @@
 # Creating a simple calculator
+
+# Function initialisation for the same.
+def calculator():
+	num1 = int(input('Enter 1st number '))
+	num2 = int(input('Enter 2nd number '))
+	print(num1)
+	print(num2)
